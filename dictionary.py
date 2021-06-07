475eb2049df65b5e3e5f4ada273f175970fc1d8f
aditya <aditya>
2021-06-07 08:19:50 +0530

    added dictionary

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+#It is unordered collection, duplicated not allowed, changagable
+#Key : value pair
+
+#name: Aditya 
+#age: 26
+#state: Maharashtra
+
+
+#empty dict
+my_first_Dict = {}
+print (my_first_Dict)
+
+#dic with values 
+
+valued_dict = {
+    "name": "Aditya",
+    "age": 26,
+    "city": "Nagpur",
+    "city": "Pune"
+}
+print (valued_dict)
+print (valued_dict["name"])
+print (valued_dict["age"])
+
+valued_dict['state'] = "Maharashtra"
+
+print (valued_dict)
+
+
+
+
+
+
