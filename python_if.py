4baf55cf4b197b117330bf7c52efb3880c792e33
aditya <aditya>
2021-06-09 08:36:40 +0530

    added if_else

---
+++ python_if.py
@@ -0,0 +1,15 @@
+x = 10 
+y = 20
+
+if x > y:
+    print ("Printed x is greater than y")
+else:
+    print ("Printed x is less than y")
+
+
+#Assignment
+# Write a program to take 4 subjects input from a student.
+# Phy, Chem, Maths, English
+# Find the %age of the student
+# if he gets more than 70% put him in grade A else grade B
+
