94f2ef2b02175f5eef8757a11451d48433d30d85
aditya <aditya>
2021-06-10 08:13:01 +0530

    added conditional with operator

---
+++ conditional_with_operators.py
@@ -0,0 +1,15 @@
+x = 100
+y = 50
+z = 1000
+
+    #True         #False
+if (x > y) and (z < x):                    
+    print ("True")
+else:
+    print ("False")
+
+     #True      #False
+if (x > y) or (z < x): 
+    print ("True")
+else: 
+    print ("False")
