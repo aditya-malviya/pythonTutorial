948ba914a0fbbdd5a36113afa1a3001911844509
aditya <aditya>
2021-06-30 08:43:35 +0530

    added write operations

--- fileOperations.py
+++ fileOperations.py
@@ -17,3 +17,18 @@ print(file1.read())
 
 file2 = open('../3.txt', 'r')
 print(file2.read(100))
+
+
+writefile = open('sample.txt', 'w')
+writefile.write("Hello Geekls")
+writefile.close()
+
+
+f = open("sample.txt", "a")
+f.write("Hello this is using append")
+f.close()
+
+
+writefile1 = open('sample1.txt', 'w')
+writefile1.write("Hello Geekls using writefile1")
+writefile1.close()
